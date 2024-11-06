425094b7dc31426ae9a1cb8fff615f53a54efe98
Rodrigo Carvalho Nogueira júnior <rodrigosms103042r6@gmail.com>
2024-11-06 07:51:26 -0300

    fixing bugs

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -5,9 +5,12 @@ import styles from '@/app/components/about/about.module.css';
 
 export default function AboutHome() {
     return (
-        <section className="flex flex-col items-center justify-center text-center p-5">
-            <h1 className="font-bold text-3xl">Sobre Mim</h1>
+        <section className={`flex flex-col items-center justify-center text-center p-5 ${styles.about_home}`}>
+           
+           <div className={`${styles.conteiner}`}>
+          
             <div className={`flex flex-col items-center justify-center ${styles.about}`} >
+            <h1 className="font-bold text-3xl">Sobre Mim</h1>
                 <div className="flex flex-col gap-5 p-4 text-center">
                     <h1 className="font-extrabold text-3xl">Rodrigo Carvalho</h1>
                     <p>Sou um Desenvolvedor de Software com 2 anos de experiência, sou um desenvolvedor Full Stack que trabalha com as linguagens Python e Javascript</p>
@@ -19,7 +22,8 @@ export default function AboutHome() {
                         Mais Sobre mim
                     </button>
                 </div>
-            <Image src={'/images/notebook.svg'}  alt={'notebook-icon'} width={1000} height={1000} />
+            </div> 
+            <Image src={'/images/notebook.svg'} className={`${styles.notebook}`}  alt={'notebook-icon'} width={1000} height={1000} />
             </div>
             
         </section>
